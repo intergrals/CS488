f905439238dadf04f400022c2991c0c3df5d2484
hlpan <hlpan@edu.uwaterloo.ca>
2018-10-22 18:41:08 -0400

    add crappy rotation test model

---
+++ A3/Assets/test_puppet.lua
@@ -0,0 +1,32 @@
+-- puppet.lua
+-- A simplified puppet without posable joints, but that
+-- looks roughly humanoid.
+
+rootnode = gr.node('root')
+rootnode:rotate('y', -20.0)
+rootnode:scale( 0.25, 0.25, 0.25 )
+rootnode:translate(0.0, 0.0, -1.0)
+
+red = gr.material({1.0, 0.0, 0.0}, {0.1, 0.1, 0.1}, 10)
+blue = gr.material({0.0, 0.0, 1.0}, {0.1, 0.1, 0.1}, 10)
+green = gr.material({0.0, 1.0, 0.0}, {0.1, 0.1, 0.1}, 10)
+white = gr.material({1.0, 1.0, 1.0}, {0.1, 0.1, 0.1}, 10)
+
+torso = gr.mesh('cube', 'torso')
+rootnode:add_child(torso)
+torso:set_material(white)
+torso:scale(0.5,1.0,0.5);
+
+leftHip = gr.joint('leftHip', { 0, 45, 90 }, {-90, 0, 90})
+torso:add_child(leftHip)
+leftHip:scale(1/0.5,1.0,1/0.5);
+leftHip:scale(0.21, 0.21, 0.21)
+leftHip:translate(-0.38, -0.5, 0.0)
+-- leftHip:set_material(blue)
+
+leftLeg = gr.mesh('cube', 'leftLeg')
+leftHip:add_child(leftLeg)
+leftLeg:scale(0.5,4,0.5)
+leftLeg:translate(0,-2.8,0)
+leftLeg:set_material(red)
+return rootnode
